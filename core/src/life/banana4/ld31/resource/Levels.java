75dcc413f5aa111eef3b6a64228fa0db35e9d101
Jonas Dann <jonas.chr.dann@gmail.com>
2014-12-06 22:08:12 +0100

    The map is kind of working

--- core/src/life/banana4/ld31/resource/Levels.java
+++ core/src/life/banana4/ld31/resource/Levels.java
@@ -45,30 +45,65 @@ public class Levels extends ResourceBag<Level>
         map.prepare();
         Pixmap pixmap = map.consumePixmap();
         Level level = new Level(pixmap.getWidth(), pixmap.getHeight(), new TiledGraph(pixmap));
+
+        int color;
         for (int x = 0; x < map.getWidth(); x++)
         {
             for (int y = 0; y < map.getHeight(); y++)
             {
-                int color = pixmap.getPixel(x, y);
+                color = pixmap.getPixel(x, y);
                 System.out.println(color);
                 //System.out.println(TileType.getType(color));
+                int xRegion = -1, yRegion = -1;
                 switch (TileType.getType(color))
                 {
                     case WALL:
                         level.addEntity(new Wall(TILE_WIDTH, TILE_WIDTH).move(x * TILE_WIDTH, y * TILE_WIDTH));
                         break;
+                    case SNOW_TOP_LEFT:
+                    xRegion = 0; yRegion = 2; break;
+                    case SNOW_TOP:
+                        xRegion = 1; yRegion = 2; break;
+                    case SNOW_TOP_RIGHT:
+                        xRegion = 2; yRegion = 2; break;
+                    case SNOW_LEFT:
+                        xRegion = 0; yRegion = 1; break;
                     case SNOW:
-                        level.getFloor().add(new FloorTile(new TextureRegion(textures.floor, 4 * TILE_WIDTH, TILE_WIDTH,
-                                                                             TILE_WIDTH, TILE_WIDTH)).setPosition(
-                            new Vector2(x * TILE_WIDTH, y * TILE_WIDTH)));
-                        break;
+                        xRegion = 4; yRegion = 1; break;
+                    case SNOW_RIGHT:
+                        xRegion = 2; yRegion = 1; break;
+                    case SNOW_BOTTOM_LEFT:
+                        xRegion = 0; yRegion = 0; break;
+                    case SNOW_BOTTOM:
+                        xRegion = 1; yRegion = 0; break;
+                    case SNOW_BOTTOM_RIGHT:
+                        xRegion = 2; yRegion = 0; break;
+                    case DIRT_TOP_LEFT:
+                        xRegion = 3; yRegion = 2; break;
+                    case DIRT_TOP:
+                        xRegion = 4; yRegion = 2; break;
+                    case DIRT_TOP_RIGHT:
+                        xRegion = 5; yRegion = 2; break;
+                    case DIRT_LEFT:
+                        xRegion = 3; yRegion = 1; break;
                     case DIRT:
-                        level.getFloor().add(new FloorTile(new TextureRegion(textures.floor, TILE_WIDTH, TILE_WIDTH,
-                                                                             TILE_WIDTH, TILE_WIDTH)).setPosition(
-                            new Vector2(x * TILE_WIDTH, y * TILE_WIDTH)));
+                        xRegion = 1; yRegion = 1; break;
+                    case DIRT_RIGHT:
+                        xRegion = 5; yRegion = 1; break;
+                    case DIRT_BOTTOM_LEFT:
+                        xRegion = 3; yRegion = 0; break;
+                    case DIRT_BOTTOM:
+                        xRegion = 4; yRegion = 0; break;
+                    case DIRT_BOTTOM_RIGHT:
+                        xRegion = 5; yRegion = 0; break;
                     case NONE:
                         System.out.println("No type for that color");
                         break;
+                }
+                if (xRegion != -1 && yRegion != -1)
+                {
+                    level.getFloor().add(new FloorTile(new TextureRegion(textures.floor, xRegion * TILE_WIDTH, yRegion * TILE_WIDTH, TILE_WIDTH,
+                                                                         TILE_WIDTH)).setPosition(new Vector2(x * TILE_WIDTH, y * TILE_WIDTH)));
                 }
             }
         }
--- core/src/life/banana4/ld31/util/TileType.java
+++ core/src/life/banana4/ld31/util/TileType.java
@@ -6,8 +6,24 @@ import java.util.Map;
 public enum TileType
 {
     WALL(255),
+    SNOW_TOP_LEFT(-370546177),
+    SNOW_TOP(-724249345),
+    SNOW_TOP_RIGHT(-1077952513),
+    SNOW_LEFT(-1431655681),
     SNOW(-1),
-    DIRT(1715024383),
+    SNOW_RIGHT(-1802201857),
+    SNOW_BOTTOM_LEFT(2139062271),
+    SNOW_BOTTOM(1785359103),
+    SNOW_BOTTOM_RIGHT(1431655935),
+    DIRT_TOP_LEFT(-118361601),
+    DIRT_TOP(1),
+    DIRT_TOP_RIGHT(-608585217),
+    DIRT_LEFT(3),
+    DIRT(-1082097409),
+    DIRT_RIGHT(4),
+    DIRT_BOTTOM_LEFT(-1672589569),
+    DIRT_BOTTOM(6),
+    DIRT_BOTTOM_RIGHT(1664234239),
     NONE(300);
 
     private static final Map<Integer, TileType> LOOKUP = new HashMap<>();
@@ -25,7 +41,7 @@ public enum TileType
 
     public static boolean isFloor(TileType type)
     {
-        return type == SNOW || type == DIRT;
+        return !(type == WALL || type == NONE);
     }
 
     static
